16ede0aca52d868825e4aa7a52f4ec44435c1c98
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-02-17 20:02:27 -0300

    final

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // ejercicio 2
 
+const nombre = "Antonio";
+const edad = 24;
+const programdor = true;
+const nacimiento = new Date(1998, 9, 10);
 const libro = {
     titulo: "Un monstruo viene a verme",
+    autor: "Patrick Ness",
+    fecha: new Date(2011, 2, 5),
+    url: "https://books.google.com.ar/books/about/Un_monstruo_viene_a_verme.html?id=vPoOAwAAQBAJ&printsec=frontcover&source=kp_read_button&hl=es-419&redir_esc=y#v=onepage&q&f=false",
 };
-const list = ["Antonio", 24, true, new Date(1998, 9, 10)];
+
+//lista de elementos
+const list = [nombre, edad, programdor, nacimiento, libro];
