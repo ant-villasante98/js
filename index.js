537fc87ecadac50760ef276f1aa4647237b4c8dd
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-02-23 13:22:23 -0300

    ejercicio 5

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let ant = "antonio";
+let alturaCm = 179;
+let alturaM = parseFloat((alturaCm / 100).toFixed(2));
+console.log(alturaM);
+let pesoKgr = 62.4;
+let alturaMR = parseFloat((alturaCm / 100).toFixed(0));
+console.log(alturaMR);
+let pesoR = parseInt(pesoKgr);
+console.log(pesoR);
 
-console.log(`hola ${ant}`);
+let max_value_JS = Number.MAX_VALUE;
+let max_mas_uno = max_value_JS + 1;
+console.log(max_value_JS);
+console.log(max_mas_uno);
+console.log("El valores son iguales? ", max_mas_uno == max_value_JS);
