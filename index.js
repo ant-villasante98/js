101bacfbdcb4344b1358e99255f957bf77361105
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-02-28 16:00:28 -0300

    ejercicio 6 terminado

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-let ant = "antonio";
+//lista de compras
+const list = ["yogur", "leche", "papel", "arroz", "azucar"];
 
-console.log(`hola ${ant}`);
+list.push("Aceite Girasol");
+console.log(list);
+list.splice(list.indexOf("Aceite Girasol"), 1);
+console.log(list);
+
+//lista de peliculas
+const peliculas = [
+    {
+        titulo: "Interestelar",
+        director: "Christopher Nolan",
+        fecha: "06-11-2014",
+    },
+    {
+        titulo: "Click: Perdiendo el control",
+        director: "Frank Coraci",
+        fecha: "17-08-2006",
+    },
+    {
+        titulo: "Soucer Code",
+        director: "Duncan Jones",
+        fecha: "01-04-2011",
+    },
+];
+
+const p_1_enero_2010 = peliculas.filter(({ fecha }) => {
+    const res = Number(fecha.slice(6)) > 2010;
+    return res;
+});
+console.log(p_1_enero_2010);
+
+const directores = peliculas.map(({ director }) => director);
+console.log(directores);
+
+const titulos = peliculas.map(({ titulo }) => titulo);
+console.log(titulos);
+
+const concat_d_t = directores.concat(titulos);
+console.log("Lista con concat: ", concat_d_t);
+
+const factor_propagacion = [...directores, ...titulos];
+console.log("Lista con factor de propagacion: ", concat_d_t);
